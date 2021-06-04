9c597c270e332a2e6e21995029e32364f81a98c7
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2021-06-04 11:03:55 +0300

    Add project files.

---
+++ C-101/Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C_101
+{
+    public class Class
+    {
+        byte b = 5;
+        sbyte c = 5;
+
+        short s = 5;
+        ushort us = 5;
+
+        Int16 i16 = 2;
+        float f = 3.4f;
+        double d = 5;
+        decimal de = 4;
+
+        string s = "deneme";
+        bool b1 = true;
+        bool b2 = false;
+        DateTime dt = DateTime.Now;
+    }
+}
